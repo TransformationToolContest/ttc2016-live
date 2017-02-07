ca2eaad8bfbf923f28cb440915f774d5e101859e
Antonio García-Domínguez <nyoescape@gmail.com>
2017-02-07 23:14:03 +0000

    SIGMA: add ScalaRunner to allow for using 'Export as runnable JAR' from Eclipse easily

---
+++ solutions/cz.cvut.fit.sigma.ttc16.live/src/main/java/cz/cvut/fit/sigma/ttc16/live/ScalaRunner.java
@@ -0,0 +1,15 @@
+package cz.cvut.fit.sigma.ttc16.live;
+
+/**
+ * Quick workaround to allow using the 'Export as runnable JAR' option in
+ * Eclipse.
+ *
+ * @author Antonio Garcia-Dominguez
+ */
+public class ScalaRunner {
+
+	public static void main(String[] args) {
+		Main.main(args);
+	}
+
+}
